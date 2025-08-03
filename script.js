a4dd8442883945afef023fe0b32b1fb56a5e0b74
joyankush <joyankushroy0987@gmail.com>
2025-08-03 14:12:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ document.getElementById("booking-form")?.addEventListener("submit", async functi
   const pickup = document.getElementById("pickup").value;
   const drop = document.getElementById("drop").value;
 
-  const res = await fetch("http://localhost:3000/api/book", {
+  const res = await fetch("https://rapido-backend-1omf.onrender.com/api/book", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ pickup, drop })
   });
+
   const data = await res.json();
   alert(data.message);
 });
